f3a23ec10c91c400698ac0bdd163c4c0f25b9731
premsarja <premsagar@prodot.in>
2024-03-07 13:51:22 +0530

    done

--- sg.tf
+++ sg.tf
@@ -1,15 +1,43 @@
-resource "aws_security_group" "a/b-public" {
+resource "aws_security_group" "alb_public" {
   name        = "roboshop allows public-alb-sg"
   description = "Allow TLS inbound traffics"
   vpc_id      =  data.terraform_remote_state.vpc.outputs.VPC_ID 
 
 
   ingress {
-    description = "docdb from private network "
+    description = "public network "
     from_port   = 80
     to_port     = 90
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-{var.ENV}-public-alb-sg"
+  }
+}
+
+# creates private security group
+
+resource "aws_security_group" "alb_private" {
+  name        = "roboshop allows public-alb-sg"
+  description = "Allow TLS inbound traffics"
+  vpc_id      =  data.terraform_remote_state.vpc.outputs.VPC_ID 
+
+
+  ingress {
+    description = " private network "
+    from_port   = 80
+    to_port     = 90
+    protocol    = "tcp"
+    cidr_blocks = ["data.terraform_re"]
   }
 
   egress {
