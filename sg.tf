becfac1d7d087d1b7515ec8f94d086907202ec9a
premsarja <premsagar@prodot.in>
2024-03-07 18:12:39 +0530

    done

--- sg.tf
+++ sg.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "alb_private" {
     from_port   = 80
     to_port     = 90
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_ID]
 
   }
 
