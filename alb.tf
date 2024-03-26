b13a7a66f73b0a74c79ce3429f6390b9239bb3ad
premsarja <premsagar@prodot.in>
2024-03-26 16:25:54 +0530

    done

--- alb.tf
+++ alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "test" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL
   load_balancer_type = "application"
-  security_groups    = var.INTERNAL ? [aws_security_group.ALB_PRIVATE_SG.id] : [aws_security_group.ALB_PUBLIC_SG.id]
+  security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb.public.id]
   subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS 
   tags = {
     Environment = "roboshop-(var.ENV)-alb"
